14d50013681b3d0ce55eec10afeebf267f709ff2
DivijeshVarma <divijeshvarma.a@pidatacenters.com>
2025-02-28 15:15:43 +0530

    changes2

--- application_start.sh
+++ application_start.sh
@@ -7,7 +7,6 @@ DOCKER_TAG=$(aws ssm get-parameter --name "/myapp/DOCKER_TAG" --query "Parameter
 DEPLOYMENT_NAME=$(aws ssm get-parameter --name "/myapp/DEPLOYMENT_NAME" --query "Parameter.Value" --output text)
 DOCKER_HUB_REPO=$(aws ssm get-parameter --name "/myapp/DOCKER_HUB_REPO" --query "Parameter.Value" --output text)
 
-source /tmp/deployment_variables.txt
 RANDOM_PORT=$(shuf -i 1024-65535 -n 1)
 
 docker run -d -p $RANDOM_PORT:8501 --name $DEPLOYMENT_NAME $DOCKER_HUB_REPO:$DOCKER_TAG
