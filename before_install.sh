6ea95638e3a50c5b9959cc15f81e3e9433ac754d
DivijeshVarma <divijeshvarma.a@pidatacenters.com>
2025-03-03 12:12:44 +0530

    new changes

--- before_install.sh
+++ before_install.sh
@@ -7,8 +7,8 @@ DEPLOYMENT_NAME=$(aws ssm get-parameter --name "/myapp/DEPLOYMENT_NAME" --query 
 
 
 echo "Stopping old Docker container if running..."
-docker ps -a --filter "name=$DEPLOYMENT_NAME" --format "{{.ID}}" | xargs -r docker stop
-docker ps -a --filter "name=$DEPLOYMENT_NAME" --format "{{.ID}}" | xargs -r docker rm
+#docker ps -a --filter "name=$DEPLOYMENT_NAME" --format "{{.ID}}" | xargs -r docker stop
+#docker ps -a --filter "name=$DEPLOYMENT_NAME" --format "{{.ID}}" | xargs -r docker rm
 
 echo "Clean up old resources if needed..."
 # Add additional cleanup commands if necessary
