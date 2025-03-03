be2fed85282af6e8823ba215d21969b397314cf2
DivijeshVarma <divijeshvarma.a@pidatacenters.com>
2025-03-03 16:37:26 +0530

    develop change

--- application_stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Stopping the Docker container..."
-#docker ps -a --filter "name=$DEPLOYMENT_NAME" --format "{{.ID}}" | xargs -r docker stop
-#docker ps -a --filter "name=$DEPLOYMENT_NAME" --format "{{.ID}}" | xargs -r docker rm
-
-echo "Docker container stopped and removed."
-
