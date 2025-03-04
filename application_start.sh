77aad0811c54c4d67934e75488a9b59b45a51559
DivijeshVarma <divijeshvarma.a@pidatacenters.com>
2025-03-04 15:01:26 +0530

    Changes

--- application_start.sh
+++ application_start.sh
@@ -4,10 +4,10 @@ echo "Starting the new Docker container..."
 
 # Fetch DOCKER_TAG and DEPLOYMENT_NAME from SSM Parameter Store
 DOCKER_TAG=$(aws ssm get-parameter --name "/myapp/DOCKER_TAG" --query "Parameter.Value" --output text)
-DEPLOYMENT_NAME=$(aws ssm get-parameter --name "/myapp/DEPLOYMENT_NAME" --query "Parameter.Value" --output text)
 DOCKER_HUB_REPO=$(aws ssm get-parameter --name "/myapp/DOCKER_HUB_REPO" --query "Parameter.Value" --output text)
 
-source /tmp/deployment_variables.txt
+DEPLOYMENT_NAME="${DOCKER_TAG}-$(date +%Y%m%d%H%M%S)"
+
 RANDOM_PORT=$(shuf -i 1024-65535 -n 1)
 
 docker run -d -p $RANDOM_PORT:8501 --name $DEPLOYMENT_NAME $DOCKER_HUB_REPO:$DOCKER_TAG
